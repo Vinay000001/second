eb4fcce5c81603e54bdffc744a65d28732c727eb
Vinay000001 <156875473+Vinay000001@users.noreply.github.com>
2024-02-20 23:31:03 +0530

    Update exp2.java

--- exp2.java
+++ exp2.java
@@ -1,7 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-        System.out.println("vinay");
+        System.out.println("vinay 22bdo10040");
     }
 }
-//updated
